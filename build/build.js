320536501972de6b4797af16fdf5cfac66e318b9
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-03 17:48:17 -0800

    updateCatchphrases correctly

--- build/build.js
+++ build/build.js
@@ -18,34 +18,25 @@ const headEl = document.getElementById('head');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const chatchphrasesEl = document.getElementById('chatchphrases');
+const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 const logoutButton = document.getElementById('logout');
 
-// we're still keeping track of 'this session' clicks, so we keep these lets
 let headCount = 0;
 let middleCount = 0;
 let bottomCount = 0;
 
-let catchphrasesArr = [];
-
 headDropdown.addEventListener('change', async() => {
-    // increment the correct count in state
     headCount++;
-    // update the head in supabase with the correct data
-    console.log(headDropdown.value);
     await updateHead(headDropdown.value);
 
-    refreshData(); // does this >> displayStats(); x fetchAndDisplayCharacter();
+    refreshData();
 });
 
 
 middleDropdown.addEventListener('change', async() => {
-    // increment the correct count in state
     middleCount++;
-    // update the middle in supabase with the correct data
-    console.log(middleDropdown.value);
     await updateMiddle(middleDropdown.value);
 
     refreshData();
@@ -53,24 +44,20 @@ middleDropdown.addEventListener('change', async() => {
 
 
 bottomDropdown.addEventListener('change', async() => {
-    // increment the correct count in state
     bottomCount++;
-    // update the bottom in supabase with the correct data
-    console.log(bottomDropdown.value);
     await updateBottom(bottomDropdown.value);
 
     refreshData();
 });
 
 catchphraseButton.addEventListener('click', async() => {
-
     // go fetch the old catch phrases
     const character = await getCharacter();
-    console.log(character);
     // update the catchphrases array locally by pushing the new catchphrase into the old array
-    catchphrasesArr.push(catchphraseInput.value);
-    console.log(catchphrasesArr);
-    updateCatchphrases(catchphrasesArr);
+    const newCatchphrase = catchphraseInput.value;
+    character.catchphrases.push(newCatchphrase);
+    await updateCatchphrases(character.catchphrases);
+    console.log(character)
     // update the catchphrases in supabase by passing the mutated array to the updateCatchphrases function
     catchphraseInput.value = '';
     refreshData();
@@ -81,19 +68,16 @@ window.addEventListener('load', async() => {
         head: 'bird',
         middle: 'blue',
         bottom: 'single leg',
-        catchphrases: catchphrasesArr
+        catchphrases: []
     };
-    console.log(catchphrasesArr);
-    // on load, attempt to fetch this user's character
+
     const userCharacter = await getCharacter();
     if (!userCharacter) {
         await createCharacter(character);
     }
-    // if this user turns out not to have a character
-    // create a new character with correct defaults for all properties (head, middle, bottom, catchphrases)
+
     // and put the character's catchphrases in state (we'll need to hold onto them for an interesting reason);
 
-    // then call the refreshData function to set the DOM with the updated data
     refreshData();
 });
 
@@ -108,25 +92,25 @@ function displayStats() {
 
 
 async function fetchAndDisplayCharacter() {
-    // fetch the character from supabase
     const character = await getCharacter(); 
-    console.log(character);
-    // if the character has a head, display the head in the dom
+
     if (character.head) {
-        // console.log(character.head);
         headEl.style.backgroundImage = `url(../assets/${character.head}-head.png)`;
     }
-    // if the character has a middle, display the middle in the dom
     if (character.middle) {
-        // console.log(character.middle);
         middleEl.style.backgroundImage = `url(../assets/${character.middle}-middle.png)`;
     }
-    // if the character has a pants, display the pants in the dom
     if (character.bottom) {
-        // console.log(character.bottom);
         bottomEl.style.backgroundImage = `url(../assets/${character.bottom}-pants.png)`;
     }
     // loop through catchphrases and display them to the dom (clearing out old dom if necessary)
+    catchphrasesEl.textContent = '';
+    for (let catchphrase of character.catchphrases) {
+        const p = document.createElement('p');
+        p.textContent = catchphrase;
+        catchphrasesEl.append(p);
+    }
+
 }
 
 function refreshData() {
